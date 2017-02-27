b0278e0ddfe06f0f65db3980534b04a97c725c7c
scratchesfirelli <scratches.firelli@gmail.com>
2017-02-27 15:15:16 +0200

    Edited register component, validation and flash-messages

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessagesService: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    //Required fields
+    if(!this.validateService.validateRegister(user)) {
+      this.flashMessagesService.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    //Validate email
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessagesService.show('Please use validate email', {cssClass: 'alert-danger', timeout: 3000});
+      return false
+    }
   }
 
 }
